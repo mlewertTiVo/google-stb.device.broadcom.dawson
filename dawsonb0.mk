abde80397122953e0964eb2c2af1c6b7840657cf
Pierre Couillaud <pierre@broadcom.com>
2017-01-30 19:07:49 -0800

    dawson: do not request legacy decoders.
    refs #SWANDROID-3767
    
    Change-Id: I8d9c61d6518103518d804dac6cb71ea153478538
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- dawsonb0.mk
+++ dawsonb0.mk
@@ -25,6 +25,9 @@ export ANDROID_SUPPORTS_WIDEVINE  := n
 export ANDROID_ENABLE_HDMI_HDCP   := n
 export ANDROID_SUPPORTS_PLAYREADY := n
 
+# no legacy decoder (vp9, h263, mpeg4) in hardware s.2
+export HW_DECODER_LEGACY_SUPPORT  := n
+
 # baseline the common support.
 $(call inherit-product, device/broadcom/common/bcm.mk)
 PRODUCT_NAME                     := dawsonb0
