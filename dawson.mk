64e3de9392c935964a5ecec9e4f66aecf4e4f41e
Jerry Wong <jerry.wong@broadcom.com>
2017-05-11 16:34:00 -0700

    dawson: enable bt now that the chip is populated
    Change-Id: Ia243ca5041b8f12b4a2145a4ac22c4fa3d1ad5c0

--- dawson.mk
+++ dawson.mk
@@ -23,9 +23,6 @@ include device/broadcom/dawson/common.mk
 LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=528m@416m
 LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=744m@944m
 
-# board missing bt, disable for now.
-export ANDROID_ENABLE_BT          := n
-
 # no legacy decoder (vp8, h263, mpeg4) in hardware s.2
 export HW_HVD_REVISION := S
 # v3d mmu available.
