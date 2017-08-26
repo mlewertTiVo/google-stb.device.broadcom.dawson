a1edf8a7da73f089ec857843164ab735b6738ab1
Pierre Couillaud <pierre@broadcom.com>
2017-08-25 19:39:19 -0700

    dawson: bootloader binary distribution integration.
    refs #SWANDROID-4317
    
    Change-Id: Ib138fac4e0b00978f24d1b08df590261b846f73c
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- dawson_nvi/AndroidBoard.mk
+++ dawson_nvi/AndroidBoard.mk
@@ -4,5 +4,8 @@ include $(LOCAL_PATH)/AndroidKernel.mk
 include $(LOCAL_PATH)/AndroidPhony.mk
 
 $(call add-radio-file, bootloader.img)
+
+ifneq ($(BCM_DIST_BLIM_BINS),y)
 $(call add-radio-file, gpt.bin)
+endif
 
