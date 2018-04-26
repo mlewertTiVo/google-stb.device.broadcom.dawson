c146ef2b33e70ba9d3d4a9b2eed33093798ff35d
Pierre Couillaud <pierre@broadcom.com>
2018-04-25 22:01:32 -0700

    dawson: adding 'nab' device profile.
    the 'nab' device is the exact same one as the 'dawson' reference
    device, except it does not provide a|b mode support.  however, all
    other characteristics of the device it derives from are kept, this
    includes: sizes of vendor|system|userdata partitions sizes, full
    treble mode only, ...
    
    since the 'base' and 'nab' devices are effectively the same and
    share the same profiles, one way to tell which version is running
    under android is to check the "ro.product.name" which will contain
    a "_nab" suffix for 'nab' devices.
    
    refs #SWANDROID-5036
    
    Change-Id: I86dfe21332e5bbfab93a4b88a06aa5c2d2532c1b
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>
    (cherry picked from commit db0442092704666c70dcf86425307d296207446f)

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,10 @@
 add_lunch_combo dawson-userdebug
 add_lunch_combo dawson-user
 
+# exact same as 'dawson' but without a|b mode support.
+add_lunch_combo dawson_nab-userdebug
+add_lunch_combo dawson_nab-user
+
 # 'nvi' targets - 'no vendor image' for legacy layout on O+
 add_lunch_combo dawson_nvi-userdebug
 add_lunch_combo dawson_nvi-user
@@ -9,4 +13,3 @@ add_lunch_combo aosp_dawson-userdebug
 
 add_lunch_combo dawson64-userdebug
 add_lunch_combo dawsonk64-userdebug
-
