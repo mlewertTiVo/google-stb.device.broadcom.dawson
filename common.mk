b308bb78dcd009b385af0197a8e6317a76e6e17f
Pierre Couillaud <pierre.couillaud@broadcom.com>
2018-06-06 03:54:46 -0700

    dawson: restructure bindist builds for source|binary co-existence.
    refs #SWANDROID-4334
    
    Signed-off-by: Pierre Couillaud <pierre.couillaud@broadcom.com>

--- common.mk
+++ common.mk
@@ -11,7 +11,6 @@ export BCM_BINDIST_BL_ROOT       := vendor/broadcom/prebuilts/bootloaders/dawson
 export BCM_BINDIST_LIBS_ROOT     := vendor/broadcom/prebuilts/nximg/4.9/dawson
 export BCM_BINDIST_KNL_ROOT      := device/broadcom/dawson-kernel/4.9
 export BCM_DIST_FORCED_BINDIST   := y
-export BCM_DIST_BLIM_BINS        := y
 
 # compile the rc's for the device.
 ifeq ($(LOCAL_DEVICE_FULL_TREBLE),y)
