701096e992dc9ff35d86e0cab94a20b1e8b07a6e
Pierre Couillaud <pierre@broadcom.com>
2018-09-18 19:53:07 -0700

    dawson_mini: fix up v3d module parameter.
    refs #SWANDROID-XXXX
    
    Change-Id: I542d8cfe417613427a76c608638f5df8ed4db76b
    Signed-off-by: Pierre Couillaud <pierre@broadcom.com>

--- dawson.mk
+++ dawson.mk
@@ -45,7 +45,7 @@ include device/broadcom/dawson/common.mk
 ifeq ($(LOCAL_DEVICE_LOWRAM),y)
 LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=146m@510m
 LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=256m@688m
-LOCAL_DEVICE_KERNEL_CMDLINE      += ignore_cma=1
+LOCAL_DEVICE_KERNEL_CMDLINE      += brcmv3d.ignore_cma=1
 else
 LOCAL_DEVICE_KERNEL_CMDLINE      += bmem=242m@414m
 LOCAL_DEVICE_KERNEL_CMDLINE      += brcm_cma=768m@1176m
